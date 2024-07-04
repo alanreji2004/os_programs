51c3fc9b032dc3410d2e0e79b2f31a4bce07257a
jarvis630 <168496656+jarvis630@users.noreply.github.com>
2024-07-04 11:48:22 +0530

    Update 20.semaphore.c

--- 20.semaphore.c
+++ 20.semaphore.c
@@ -1,54 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h>
 
-sem_t mutex, full, empty;
-int buff[10];
-int in = 0, out = 0;
+#define BUFFER_SIZE 5
+#define MAX_ITEMS 20
 
-void* producer(void* arg) {
-    int item;
-    while (1) {
+int buffer[BUFFER_SIZE];
+int in = 0;
+int out = 0;
+int produced_count = 0;
+int consumed_count = 0;
+
+sem_t mutex;
+sem_t full;
+sem_t empty;
+
+void *producer(void *arg)
+{
+    int item = 1;
+
+    while (produced_count < MAX_ITEMS)
+    {
         sem_wait(&empty);
         sem_wait(&mutex);
-        printf("\nEnter an item\n");
-        scanf("%d", &item);
-        buff[(in + 1) % 10] = item;
-        in = (in + 1) % 10;
+
+        buffer[in] = item;
+        printf("\nProduced: %d", item);
+        item++;
+        in = (in + 1) % BUFFER_SIZE;
+
+        produced_count++;
+
         sem_post(&mutex);
         sem_post(&full);
     }
+
+    pthread_exit(NULL);
 }
 
-void* consumer(void* arg) {
-    int item;
-    while (1) {
+void *consumer(void *arg)
+{
+    while (consumed_count < MAX_ITEMS)
+    {
         sem_wait(&full);
         sem_wait(&mutex);
-        printf("\nConsumed item is: %d\n", buff[(out + 1) % 10]);
-        out = (out + 1) % 10;
+
+        int item = buffer[out];
+        printf("\nConsumed: %d", item);
+        out = (out + 1) % BUFFER_SIZE;
+
+        consumed_count++;
+
         sem_post(&mutex);
         sem_post(&empty);
     }
+
+    pthread_exit(NULL);
 }
 
-int main() {
+int main()
+{
+    pthread_t producerThread, consumerThread;
+
     sem_init(&mutex, 0, 1);
     sem_init(&full, 0, 0);
-    sem_init(&empty, 0, 10);
+    sem_init(&empty, 0, BUFFER_SIZE);
 
-    pthread_t t1, t2;
-    pthread_create(&t1, NULL, producer, NULL);
-    sleep(2);
-    pthread_create(&t2, NULL, consumer, NULL);
+    pthread_create(&producerThread, NULL, producer, NULL);
+    pthread_create(&consumerThread, NULL, consumer, NULL);
 
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    pthread_join(producerThread, NULL);
+    pthread_join(consumerThread, NULL);
 
     sem_destroy(&mutex);
-    sem_destroy(&empty);
     sem_destroy(&full);
+    sem_destroy(&empty);
 
     return 0;
 }
